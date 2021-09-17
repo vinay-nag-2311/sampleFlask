5ba39b6847d37b93b9e1bd5e3760b178f9a2e918
A3469 <vinay.nag@axtria.com>
2021-09-17 16:48:17 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker kill flask-container
-docker rm flask-container
+#docker kill flask-container
+#docker rm flask-container
 docker run -it --name flask-container -p 8080:8080 flask-test bash
