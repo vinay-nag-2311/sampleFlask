b7cddba8405473268fbd571abae2fe09d54ca0fe
A3469 <vinay.nag@axtria.com>
2021-09-17 16:55:15 +0530

    Updated port in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker kill flask-container
 #docker rm flask-container
-docker run -i --name flask-container -p 8080:8080 flask-test bash
+docker run -i --name flask-container -p 5000:5000 flask-test bash
