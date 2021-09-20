d0b486cd16cb232394759c046022fd6a7b7a3511
A3469 <vinay.nag@axtria.com>
2021-09-20 19:21:43 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop flask-container
 docker rm flask-container
-docker run --name flask-container -p 5000:5000 flask-image-new
+docker run -d --name flask-container -p 5000:5000 flask-image-new
